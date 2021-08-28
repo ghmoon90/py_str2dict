4f57ea03674eecbc18d1aa6ca0d10040d516cee3
ghmoon90 <ghmoon090@gmail.com>
2021-08-29 04:22:11 +0900

    Update pystr2dict.py

--- pystr2dict.py
+++ pystr2dict.py
@@ -1,8 +1,13 @@
+#%%
 def dict2file( src, szfile ):
   with open(szfile,mode='wb') as f:
     f.write( str(src).encode('utf-8'))
 
 def file2dict(szfile):
+
   with open(szfile,mode='r') as f:
-    exec( 'temp_dict=' + f.read())
-  return temp_dict
+    cmd = 'temp_dict=' + f.read()
+
+  exec( cmd )
+
+  return  locals()['temp_dict']
